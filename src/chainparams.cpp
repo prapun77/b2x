9d44c1b146fb0e78c651f3cbf53577f7d69f510c
prapun77 <prapun77@gmail.com>
2018-08-04 10:47:34 +0700

    parameter

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -48,8 +48,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  */
 static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
-   // const char* pszTimestamp = "The Times 28/July/2018 Thai King Maha Vajiralongkorn's 66th birthday celebrated in Bangkok";
-    const char* pszTimestamp = "The Times 03/Jan/2009 Chancellor on brink of second bailout for banks";
+    const char* pszTimestamp = "The Time 28/07/2018  His Majesty King Maha Vajiralongkorn Bodindradebayavarangkun";
     const CScript genesisOutputScript = CScript() << ParseHex("04678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5f") << OP_CHECKSIG;
     return CreateGenesisBlock(pszTimestamp, genesisOutputScript, nTime, nNonce, nBits, nVersion, genesisReward);
 }
@@ -86,7 +85,7 @@ public:
         consensus.nPowTargetSpacing = 15;
         consensus.nPosTargetSpacing = 60;
         consensus.nBtcPowTargetSpacing = 60;
-        consensus.fPowAllowMinDifficultyBlocks = false;
+        consensus.fPowAllowMinDifficultyBlocks = true;
         consensus.fPowNoRetargeting = false;
         consensus.nRuleChangeActivationThreshold = 108; // 95% of 2016
         consensus.nMinerConfirmationWindow = 144; // nPowTargetTimespan / nPowTargetSpacing
@@ -143,10 +142,10 @@ public:
         nDefaultPort = 8995;
         nPruneAfterHeight = 100000;
 
-        genesis = CreateGenesisBlock(1231006505, 2083236893, 0x1d00ffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1532485805, 2694997395 , 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+        assert(consensus.hashGenesisBlock == uint256S("0x000000000ff4e70d6a0760e248787944e278e082d02f9cf174b0c5ef421f5049"));
+        assert(genesis.hashMerkleRoot == uint256S("0x155a90924be24bc73b56d7a62f594e76b36e9852749f90f7d6d9659b97dba1dd"));
 
         // Note that of those with the service bits flag, most only support a subset of possible options
       //  vSeeds.emplace_back("node1.b2x-segwit.io", false);
@@ -167,7 +166,7 @@ public:
 
         checkpointData = (CCheckpointData) {
             {
-                { 0, uint256S("0x000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f")},
+                { 0, uint256S("0x000000000ff4e70d6a0760e248787944e278e082d02f9cf174b0c5ef421f5049")},
             }
         };
 
@@ -251,17 +250,17 @@ public:
         nDefaultPort = 18995;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1296688602, 414098458, 0x1d00ffff, 1, 50 * COIN);
+		genesis = CreateGenesisBlock(1532485805, 2694997395 , 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x000000000933ea01ad0ee984209779baaec3ced90fa3f408719526f8d77f4943"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+        assert(consensus.hashGenesisBlock == uint256S("000000000ff4e70d6a0760e248787944e278e082d02f9cf174b0c5ef421f5049"));
+        assert(genesis.hashMerkleRoot == uint256S("155a90924be24bc73b56d7a62f594e76b36e9852749f90f7d6d9659b97dba1dd"));
 
         vFixedSeeds.clear();
         vSeeds.clear();
         // nodes with support for servicebits filtering should be at the top
-        vSeeds.emplace_back("node1.b2x-segwit.io", true);
-        vSeeds.emplace_back("node2.b2x-segwit.io", true);
-        vSeeds.emplace_back("node3.b2x-segwit.io", true);
+        //vSeeds.emplace_back("node1.b2x-segwit.io", true);
+        //vSeeds.emplace_back("node2.b2x-segwit.io", true);
+        //vSeeds.emplace_back("node3.b2x-segwit.io", true);
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
@@ -358,10 +357,11 @@ public:
         nDefaultPort = 18994;
         nPruneAfterHeight = 1000;
 
-        genesis = CreateGenesisBlock(1296688602, 2, 0x207fffff, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1532485805, 2694997395 , 0x1d00ffff, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206"));
-        assert(genesis.hashMerkleRoot == uint256S("0x4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b"));
+        assert(consensus.hashGenesisBlock == uint256S("0x000000000ff4e70d6a0760e248787944e278e082d02f9cf174b0c5ef421f5049"));
+        assert(genesis.hashMerkleRoot == uint256S("0x155a90924be24bc73b56d7a62f594e76b36e9852749f90f7d6d9659b97dba1dd"));
+
 
         vFixedSeeds.clear(); //!< Regtest mode doesn't have any fixed seeds.
         vSeeds.clear();      //!< Regtest mode doesn't have any DNS seeds.
@@ -372,7 +372,7 @@ public:
 
         checkpointData = (CCheckpointData) {
             {
-                {0, uint256S("0f9188f13cb7b2c71f2a335e3a4fc328bf5beb436012afca590b1a11466e2206")},
+                {0, uint256S("0x000000000ff4e70d6a0760e248787944e278e082d02f9cf174b0c5ef421f5049")},
             }
         };
 
